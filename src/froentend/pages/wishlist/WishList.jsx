0cedd7e23226ae0a9d10cc553c12d29461b0d673
prathmesh <prathmeshjagtap405@gmail.com>
2022-03-16 21:48:16 +0530

    feat -added wishList Page

--- src/froentend/pages/wishlist/WishList.jsx
+++ src/froentend/pages/wishlist/WishList.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Navbar, WishListCard } from "../../components";
+import "./wishList.css";
+
+const axios = require("axios").default;
 
 function WishList() {
-	return <div>WishList</div>;
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await axios.get("/api/products");
+				if (response.status === 200) {
+					setProducts([...response.data.products]);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, []);
+
+	return (
+		<div>
+			<Navbar />
+			<main className="wishlist__container">
+				<h1 className="wishlist__name">My Wishlist</h1>
+				<div className="wishlist">
+					{products &&
+						products.map((product) => (
+							<WishListCard key={product.id} product={product} />
+						))}
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export { WishList };
