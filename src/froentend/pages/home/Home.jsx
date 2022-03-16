e899bc61b1f4f33eaa8a2d8a3643c597bc6ae83e
prathmesh <prathmeshjagtap405@gmail.com>
2022-03-16 12:46:38 +0530

    Update Home.jsx

--- src/froentend/pages/home/Home.jsx
+++ src/froentend/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div>
+			Home
+			<Link to="/products">Products</Link>
+		</div>
+	);
 }
 
 export { Home };
