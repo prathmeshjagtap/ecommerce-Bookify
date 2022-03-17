1f8afa191740e0c3b1d3d41cb346a33e9e0ac783
prathmesh <prathmeshjagtap405@gmail.com>
2022-03-17 12:16:56 +0530

    fix - fixed frontend typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { FilterProvider } from "./froentend/contexts";
+import { FilterProvider } from "./frontend/contexts";
 
 // Call make Server
 makeServer();
