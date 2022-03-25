29ecd107590f021a00d32d9affdc928af94a6b71
prathmesh <prathmeshjagtap405@gmail.com>
2022-03-25 10:38:12 +0530

    fix- fixed navigation sticky issue

--- src/frontend/pages/cart/BillCard.jsx
+++ src/frontend/pages/cart/BillCard.jsx
@@ -17,45 +17,43 @@ function BillCard() {
 	const totalAmount = totalPrice + deliveryCharges;
 
 	return (
-		<div>
-			<div className="cart__bill">
-				<h2>Price Details</h2>
-				<ul className="price__details">
-					<li>
-						<p>Price({cart.length} items)</p>
-						<p>
-							<i className="fas fa-rupee-sign fa-1x"></i>
-							{totalPreviousPrice}
-						</p>
-					</li>
-					<li>
-						<p>Discount</p>
-						<p>
-							-<i className="fas fa-rupee-sign fa-1x"></i>
-							{totaldiscount}
-						</p>
-					</li>
-					<li>
-						<p>Delivery Charges</p>
-						<p>
-							<i className="fas fa-rupee-sign fa-1x"></i>
-							{deliveryCharges}
-						</p>
-					</li>
-					<li>
-						<p>Total Amount</p>
-						<p>
-							<i className="fas fa-rupee-sign fa-1x"></i>
-							{totalAmount}
-						</p>
-					</li>
-				</ul>
-				<p className="bill__save">
-					You will save <i className="fas fa-rupee-sign fa-1x"></i>
-					{totaldiscount} on this order
-				</p>
-				<button className="btn btn-primary card__btn">Place Order</button>
-			</div>
+		<div className="cart__bill">
+			<h2>Price Details</h2>
+			<ul className="price__details">
+				<li>
+					<p>Price({cart.length} items)</p>
+					<p>
+						<i className="fas fa-rupee-sign fa-1x"></i>
+						{totalPreviousPrice}
+					</p>
+				</li>
+				<li>
+					<p>Discount</p>
+					<p>
+						-<i className="fas fa-rupee-sign fa-1x"></i>
+						{totaldiscount}
+					</p>
+				</li>
+				<li>
+					<p>Delivery Charges</p>
+					<p>
+						<i className="fas fa-rupee-sign fa-1x"></i>
+						{deliveryCharges}
+					</p>
+				</li>
+				<li>
+					<p>Total Amount</p>
+					<p>
+						<i className="fas fa-rupee-sign fa-1x"></i>
+						{totalAmount}
+					</p>
+				</li>
+			</ul>
+			<p className="bill__save">
+				You will save <i className="fas fa-rupee-sign fa-1x"></i>
+				{totaldiscount} on this order
+			</p>
+			<button className="btn btn-primary card__btn">Place Order</button>
 		</div>
 	);
 }
