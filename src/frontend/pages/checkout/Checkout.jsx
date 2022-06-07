d947757cb15d9586eca6524849c96f18477faf24
prathmesh <prathmeshjagtap405@gmail.com>
2022-06-07 22:27:24 +0530

    Update Checkout.jsx

--- src/frontend/pages/checkout/Checkout.jsx
+++ src/frontend/pages/checkout/Checkout.jsx
@@ -23,68 +23,6 @@ function Checkout() {
 		<div>
 			<Navbar />
 			<Address />
-			{/* <div>
-				{address?.length > 0 ? (
-					<>
-						<button
-							className="btn btn-primary btn_add_address"
-							onClick={() => {
-								setIsModalOpen((isModalOpen) => !isModalOpen);
-							}}
-						>
-							Add New Address
-						</button>
-						<div className="address__container">
-							{address.map((location) => (
-								<div className="">
-									<input type="radio" />
-									<div className="address__card" key={location?._id}>
-										<p>{location?.name}</p>
-										<span>{location?.area}</span>
-										<span>{location?.locality}</span>
-										<p>
-											{location?.city} , {location?.pincode} , {location?.state}
-										</p>
-										<p>{location?.mobile}</p>
-										<p>{location?.alternatePhoneNumber}</p>
-										<div className="address__icons">
-											<button
-												className="btn btn-primary"
-												onClick={() => {
-													setIsEdit(true);
-													setModalAddress(location);
-													setIsModalOpen((isModalOpen) => !isModalOpen);
-												}}
-											>
-												Edit Address
-											</button>
-										</div>
-									</div>
-								</>
-							))}
-						</div>
-					</>
-				) : (
-					<div className="empty__address__container">
-						<h4>You do not have any Address, Add Address</h4>
-						<button
-							className="btn btn-primary btn_add_address"
-							onClick={() => {
-								setIsModalOpen((isModalOpen) => !isModalOpen);
-							}}
-						>
-							Add New Address
-						</button>
-					</div>
-				)}
-
-				<AddressModal
-					isEdit={isEdit}
-					address={modalAddress}
-					modalOpen={isModalOpen}
-					closeModal={() => setIsModalOpen(false)}
-				/>
-			</div> */}
 		</div>
 	);
 }
